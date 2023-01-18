7c895dd08faa3a0452ff7dcd28fda7c45be8eac6
MatiBas <mwbasauri@gmail.com>
2023-01-18 19:12:00 -0300

    Estilos del navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 import "./navbar.css";
 import logo from "../../assets/icons/logo.png";
 import usuario from "../../assets/icons/usuario.png";
 import buscar from "../../assets/icons/buscar.png";
+import carro from "../../assets/icons/carro.png";
 
 const Navbar = () => {
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           <img className='image-logo' src={logo} alt='logo'/>
           <span className="navbar-brand mx-3">Restaurant Rolling</span>
@@ -15,19 +17,16 @@ const Navbar = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <div className='container d-flex justify-content-end'>
+            <div className='container-fluid d-flex justify-content-end'>
               <img className='logo-usuario mx-2' src={usuario} alt='usuario'/>
-              <img className='logo-usuario mx-2' src={buscar} alt='buscar'/>
+              <img className='logo-usuario nav-icons-size mx-2' src={buscar} alt='buscar'/>
+              <img className='logo-carro mx-2' src={carro} alt='carro'/>
             </div>
-            {/* <form className="d-flex ms-auto" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button classNameName="btn btn-outline-success" type="submit">Search</button>
-            </form> */}
-            </div>
+          </div>
         </div>
       </nav>
     </>
-  )
-}
+  );
+}; 
 
 export default Navbar;
