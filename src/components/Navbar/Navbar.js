ef8e07c714a0c66910bd4838a4ab398ad462af5d
MatiBas <mwbasauri@gmail.com>
2023-01-18 17:18:32 -0300

    Primeros estilos del navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,29 @@
-import React from 'react'
+import React from 'react';
+import "./navbar.css";
+import logo from "../../assets/icons/logo.png";
+import usuario from "../../assets/icons/usuario.png";
+import buscar from "../../assets/icons/buscar.png";
 
 const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Restaurant Rolling</a>
+          <img className='image-logo' src={logo} alt='logo'/>
+          <span className="navbar-brand mx-3">Restaurant Rolling</span>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
+            <div className='container d-flex justify-content-end'>
+              <img className='logo-usuario mx-2' src={usuario} alt='usuario'/>
+              <img className='logo-usuario mx-2' src={buscar} alt='buscar'/>
+            </div>
+            {/* <form className="d-flex ms-auto" role="search">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button classNameName="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
+            </form> */}
+            </div>
         </div>
       </nav>
     </>
