62f9806e42fcb17b261274775b1dc945515f7b8b
Andrea Azalot <jazan1974@hotmail.com>
2023-02-02 17:25:11 -0300

    comenzando con desarrollo de la pagina de inicio

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import './App.css';
 
 function App() {
   return (
-    <>
-    </>
-    
+    <div class="card text-bg-dark">
+  <img src="https://images.pexels.com/photos/15141035/pexels-photo-15141035.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img" alt="imagen" />
+  <div class="card-img-overlay">
+    <h1 class="card-title">Busca Tu Plato Preferido</h1>
+    <div class="form">
+  <label for="formGroupExampleInput" class="form-label"></label>
+  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Escribe el nombre del plato"></input> </div>
+  </div>
+  <div class="card-body bg-warning">
+    <h1 class="titulo">Nuestros Platos</h1>
+    <p class="card-text">......</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+</div>
+
+
   );
 }
 
